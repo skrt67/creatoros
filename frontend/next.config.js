b91405e5cbfa976a71e12f23f06a7d9a6be857b4
DEPELI ALTAN <altan.depeli@etu.unistra.fr>
2025-10-30 14:30:29 +0100

    Add CSP header for NextAuth and Google OAuth

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -52,6 +52,10 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin'
           },
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com; frame-src 'self' https://accounts.google.com;"
+          },
         ],
       },
       {
