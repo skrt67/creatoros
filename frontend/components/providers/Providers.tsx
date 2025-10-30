5e4ffcbf667783f3d0beae4f3f0360bf80b451fa
DEPELI ALTAN <altan.depeli@etu.unistra.fr>
2025-10-30 14:46:46 +0100

    Add SessionProvider to Providers component

--- frontend/components/providers/Providers.tsx
+++ frontend/components/providers/Providers.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <LanguageProvider>
-      {children}
-    </LanguageProvider>
+    <SessionProvider>
+      <LanguageProvider>
+        {children}
+      </LanguageProvider>
+    </SessionProvider>
   );
 }
